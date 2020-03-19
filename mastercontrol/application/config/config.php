70b53294ec052fb67b91b6687a3c428c5e0cc5bc
Zulita123 <42364527+Zulita123@users.noreply.github.com>
2020-03-19 14:58:05 +0700

    Update config.php on mastercontrol

--- mastercontrol/application/config/config.php
+++ mastercontrol/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.4.5:81/afass/mastercontrol';
+$config['base_url'] = 'http://localhost:81/afass/mastercontrol';
 
 /*
 |--------------------------------------------------------------------------
